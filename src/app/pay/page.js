34f12913ba2496dfe8158ca8202bfe438336c3ab
vij56 <vijaydevganiya14321@gmail.com>
2023-09-20 19:09:18 +0530

    implemented stripe successfully

--- src/app/pay/page.js
+++ src/app/pay/page.js
@@ -1,7 +1,19 @@
+"use client"
+import { checkout } from "../../../utils/checkout"
+
 export default function Pay() {
   return (
     <div>
-      <h1>PAY NOW</h1>
+      <button onClick={() => {
+        checkout({
+          lineItems: [
+            {
+              price: "price_1NsQOQSHhVqEBIL0SE8EMaT8",
+              quantity: 1
+            }
+          ]
+        })
+      }}>PAY NOW</button>
     </div>
   )
 }
